f2c766277d761598925dad2e0d06172a1bca0fa4
Filbert Florenza <filbert@grip-principle.com>
2024-09-18 21:19:30 +0700

    add tekpro pertemuan 3

---
+++ Semester 1/Teknik Pemrograman/Pertemuan 3/Tugas/1.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def check_npm():
+    npm = entry.get()
+    if npm == "130029":
+        messagebox.showinfo("Nama","Dinda")
+    elif npm == "130102":
+        messagebox.showinfo("Nama","Rino")
+    else:
+        messagebox.showinfo("Error","Tidak terdaftar")
+
+root = tk.Tk()
+root.title("Cek NPM")
+root.geometry("300x200")
+
+label = tk.Label(root, text="Masukkan NPM")
+label.pack(pady=10)
+
+entry = tk.Entry(root)
+entry.pack(pady=10)
+
+button = tk.Button(root,text="Cek",command=check_npm)
+button.pack(pady=20)
+
+root.mainloop()
+
+
+
+
+
+
---
+++ Semester 1/Teknik Pemrograman/Pertemuan 3/Tugas/2.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def check_nilai():
+    try:
+        nilai = int(entry.get())
+        if nilai >=86 and nilai <= 100:
+            result = "Sangat Baik"
+        elif nilai >= 76 and nilai <= 85:
+            result = "Baik"
+        elif nilai >= 66 and nilai <= 75:
+            result = "Cukup"
+        elif nilai >= 56 and nilai <= 65:
+            result = "Kurang"
+        else:
+            result = "Gagal"
+
+        messagebox.showinfo("Evaluasi", f'Nilai: {nilai}\nEvaluasi: {result}\n')
+    except ValueError:
+        messagebox.showinfo("Error","Masukkan nilai yang valid")
+
+root = tk.Tk()
+root.title("Cek Nilai")
+root.geometry("300x200")
+
+label = tk.Label(root, text="Masukkan Nilai")
+label.pack(pady=10)
+
+entry = tk.Entry(root)
+entry.pack(pady=10)
+
+button = tk.Button(root,text="Evaluasi",command=check_nilai)
+button.pack(pady=20)
+
+root.mainloop()
