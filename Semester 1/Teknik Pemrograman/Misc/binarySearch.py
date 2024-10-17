4cf767cff560c08beb273e06c491e11e0a83d139
Filbert Florenza <filbert@grip-principle.com>
2024-10-17 16:24:49 +0700

    add binarySearch example

--- Semester 1/Teknik Pemrograman/Misc/binarySearch.py
+++ Semester 1/Teknik Pemrograman/Misc/binarySearch.py
@@ -8,10 +8,21 @@ def main():
 
 # Get first and second number
 def get_numbers():
+    '''
+        Function to get number range, looping if the user input invalid number
+
+        Input:
+            string: the string to be the user's response based on the guess number. Accept: h, l, or c
+        
+        Returns:
+            firstNumber (int): number to be the floor of the number range
+            secondNumber (int) : number to tbe ceiling of the number range
+    '''
     while True:
         try:
             firstNumber = int(input("Input first number: "))
             secondNumber = int(input("Input second number: "))
+            # First number cannot be higher than second number
             if(firstNumber > secondNumber):
                 print("First number cannot be higher than second number")
                 continue
@@ -23,27 +34,45 @@ def get_numbers():
             sys.exit()
     return firstNumber,secondNumber
 
-# Binary Search
 def get_guess(low,high):
+    '''
+        Function to get user's secret number, looping until the function get the correct number
+
+        Parameters:
+            low (int): Number to be the floor of the number range
+            high (int): Number to be the ceiling of the number range
+        
+        Input:
+            string: the string to be the user's response based on the guess number. Accept: h, l, or c
+        
+        Returns:
+            guess (float): The correct number guessed by the function
+    '''
+    # First guess, get middle number between low and high
     guess = (low+high)/2
+    # Loop until we get the correct guess
     while(True):
         try:
             print(f"\nIs your secret number {guess:.0f}?")
             print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.",end='')
             indicator = str(input()).lower()
+
             if(indicator != 'h' and indicator != 'l' and indicator != 'c'):
-                print("Input must h, l, or c")
+                raise ValueError
                 continue
+            # If guess is too high, set high to guess, then middle number again
             if(indicator == 'h'):
                 high = guess
                 guess = (low+high)/2
+            # If guess is too high, set low to guess, then middle number again
             elif(indicator == 'l'):
                 low = guess
                 guess = (low+high)/2
+            # If guess correct, return guess number while breaking the loop
             else:
                 return guess
         except ValueError:
-            print("Input must be a string")
+            print("Input must be a h, l, or c")
             pass
         except KeyboardInterrupt:
             sys.exit()
